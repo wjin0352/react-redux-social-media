eda204437b6272d01aa8efb3f8631b99cc1452b7
won <wjin0352@gmail.com>
2016-10-01 22:45:01 -0400

    added registration and login components

---
+++ components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <h3>Log in:</h3>
+      <form className='login-form'>
+        <input></input>
+        <input></input>
+        <input></input>
+      </form>
+    </div>
+  )
+}
+
+export default Login;
---
+++ components/Registration.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Registration = () => {
+  return (
+    <div className='registration'>
+      <h3>Register:</h3>
+      <form className='register-form'>
+        <input></input>
+        <input></input>
+        <input></input>
+      </form>
+    </div>
+  )
+}
+
+export default Registration;
