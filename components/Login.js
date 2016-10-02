2fcd5ccb543601df4c7e035e6fbac63740786b0a
won <wjin0352@gmail.com>
2016-10-02 00:12:10 -0400

    login reg components but fixing bug

---
+++ components/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <h3>Log please</h3>
+      <form className='login-form'>
+        <fieldset>
+          <legend>Login: </legend>
+          <li>username</li>
+            <input type='text' name='username'/><br/>
+          <li>password</li>
+            <input type='text' name='password'/><br/>
+          <input type="submit" value="done"/>
+        </fieldset>
+      </form>
+    </div>
+  )
+}
+
+export default Login;
