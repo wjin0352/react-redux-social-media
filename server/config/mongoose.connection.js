511b8be70f11ace48d80a53faf49315f580aa3e9
won <wjin0352@gmail.com>
2016-11-17 02:11:30 -0500

    added database config env variable

--- server/config/mongoose.connection.js
+++ server/config/mongoose.connection.js
@@ -5,7 +5,14 @@ const config = require('./variables.express');
 // you setup this only one time before the connexion so after you can work with promise on model instance or class instance
 mongoose.Promise = global.Promise;
 // initial connection to mongo database
-mongoose.connect(config.MONGODB.URI);
+// mongoose.connect(config.MONGODB.URI);
+mongoose.connect(process.env.MONGODB_URI, (err, db) => {
+  if(err) {
+    console.log('Error connecting to database');
+    process.exit(1);
+  }
+  console.log('Successfully connected to database...')
+});
 
 // We have a pending connection to the database running on localhost. We now need to get notified if we connect successfully or if a connection error occurs:
 const db = mongoose.connection
--- server/server.js
+++ server/server.js
@@ -6,11 +6,10 @@ const variables = require('./config/variables.express');
 require('./config/mongoose.connection');
 // set up app with middleware
 require('./config/config.express')(app);
-// setup app with routers
+// setup app with route end points
 require('./config/routes.express')(app);
 
+// application server
 app.listen(variables.EXPRESS_PORT, function() {
   console.log(variables.EXPRESS_LISTEN_MESSAGE + variables.EXPRESS_PORT);
 });
-
-
